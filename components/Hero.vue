e035927b9dd4e14a793b00ed4ae37ca730544129
marianwx <maransmk69@gmail.com>
2024-02-24 18:18:00 +0200

    .

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,28 @@
 <template>
-    <main class="h-screen flex flex-col justify-center items-center bg-purple-200 relative overflow-hidden">
-      
+  <main class="h-screen flex flex-col justify-center items-center bg-purple-200 relative overflow-hidden">
+    <main class="flex w-full flex-col items-center justify-center text-center px-4">
+      <a href="#" target="_blank" rel="noreferrer"
+        class="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"></a>
+      <h1 class="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">MXN On The <span class="text-purple-600" id="text-change">Go</span></h1>
+      <p class="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">Want to style up your photos? Maybe get that
+        perfect LinkedIn headshot? Let our AI do its magic and replace your background with your color of choice. 100%
+        free – pop your photos today.</p>
+      <a class="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 transition duration-300 hover:opacity-80" href="#">Explore →</a>
     </main>
-  </template>
+  </main>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const consoleLog = document.getElementById('text-change');
+    const texts = ['Go', 'USD', 'EUR']; // Add your desired texts here
+    let index = 1; // Start with the second text
+
+    setInterval(() => {
+      consoleLog.textContent = `${texts[index]}`;
+      index = (index + 1) % texts.length;
+    }, 3000); // Change the duration as needed (currently set to 5 seconds)
+  }
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'MXN',
-      link: [{ rel: 'icon', type: 'image/png', href: 'https://cdn.discordapp.com/attachments/1208411877198266379/1208411920500265000/MXN-removebg-preview.png?ex=65e3304d&is=65d0bb4d&hm=9b49c64eed3aa79ad35946e4ed58a07ca508516339254e11f019b44f2f6fc41f&' }],
+      link: [{ rel: 'icon', type: 'image/png', href: 'http://tinyurl.com/bd8a4997' }],
     }
   },
 })
