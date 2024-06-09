11ddee0c322eba4aefa3c6acf4ae9bf5195681d3
.maran <maransmk69@gmail.com>
2024-06-09 14:28:57 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,8 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
-      title: 'MXN',
-      link: [{ rel: 'icon', type: 'image/png', href: 'https://cdn.discordapp.com/attachments/1208411877198266379/1210973800976490556/MXN.png?ex=666674fc&is=6665237c&hm=ded677585d8d0459e0b73cf49ca3f089586c578c8c5723f13fc17086af0263e5&' }],
+      title: 'MXN Lab',
+      link: [{ rel: 'icon', type: 'image/png', href: '/MXN.png' }],
     }
   },
 })
