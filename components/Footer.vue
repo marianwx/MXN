fdd0120849401ae0fbfa2720962e2a7b77729d28
.maran <maransmk69@gmail.com>
2024-07-04 02:41:07 +0300

    .

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="mx-auto bg-purple-200 dark:bg-indigo-900 w-full max-w-container px-4 sm:px-6 lg:px-8" @click="hideTooltip">
       <div class="border-t border-slate-900/5 dark:border-slate-100/5 py-10">
         <div class="flex justify-center">
-          <img class="w-32" src="https://cdn.discordapp.com/attachments/1208411877198266379/1208412055883878421/MXN-removebg-preview.png?ex=665d232d&is=665bd1ad&hm=353c383c625511c129c0d37019cae8df71bda0e68855e9e5bc404008b429405d&" alt="">
+          <img class="w-32" src="/mxn-mini.png" alt="">
         </div>
         <p class="mt-5 text-center text-sm leading-6 text-slate-500 dark:text-slate-300">© 2024 MXN Lab. All rights reserved.</p>
         <div class="mt-8 flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700">
